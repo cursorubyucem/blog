d3b7de3c550cfeccb040e6c6beb454cd14ec8537
Juan Evangelista Fletes Garcia <cursorubyucem@gmail.com>
2017-01-14 09:40:22 -0800

    vistas

--- app/controllers/practica_variables_controller.rb
+++ app/controllers/practica_variables_controller.rb
@@ -1,4 +1,49 @@
 class PracticaVariablesController < ApplicationController
   def index
+  	@titulo = 'Practica variables de instancia'
+  	opcion = 3 
+  	
+  	#@mensaje = ''
+  	#case opcion
+  	#when 1
+  	#	@mensaje = 'Servicio al cliente'
+  	#when 2
+  	#	@mensaje = 'ventas'
+  	#when 3
+  	#	@mensaje = 'reclamos'
+  	#else
+    #	@mensaje = 'no existe esa opción'
+  	#end
+
+  	#@mensaje = case opcion
+	#		  	when 1
+	#		  		'Servicio al cliente'
+	#		  	when 2
+	#		  		'ventas'
+	#		  	when 3
+	#		  		'reclamos'
+	#		  	else
+	#		  		'no existe esa opción'
+	#		  	end
+
+	@mensaje = 'Se guardo exitosamente!!'
+	
+	mensaje_opcion(opcion)
+
   end
+
+  def mensaje_opcion(opcion)
+  	@mensaje_opcion_seleciona = case opcion
+			  	when 1
+			  		'Servicio al cliente'
+			  	when 2
+			  		'ventas'
+			  	when 3
+			  		'reclamos'
+			  	else
+			  		'no existe esa opción'
+			  	end
+
+  end
+
 end
